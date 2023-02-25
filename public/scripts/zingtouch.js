8c9d25976def31f5c0cbb3cddc9eba3e6a00e506
Kartik Gandhi <gandhikartik73@gmail.com>
2023-02-25 23:08:02 +0530

    add menu items and change active item on wheel rotation

--- public/scripts/zingtouch.js
+++ public/scripts/zingtouch.js
@@ -1,8 +1,15 @@
-const wheelContainer = $('#wheel-container')[0];
 const wheelRegion = $('#wheel')[0];
+const menuItems = $('.menu-item');
 
-const zt = new ZingTouch.Region(wheelContainer);
+const zt = new ZingTouch.Region(wheelRegion);
 
 zt.bind(wheelRegion, 'rotate', function(e){
-    console.log(Math.floor(e.detail.distanceFromOrigin));
+
+    if((!(Math.floor(e.detail.distanceFromOrigin) % 45)) && (Math.floor(e.detail.distanceFromOrigin) + 1 > Math.floor(e.detail.distanceFromOrigin))){
+        let currActiveId = parseInt($('.active')[0].dataset.itemid);
+        currActiveId = (++currActiveId) % menuItems.length;
+        $('.active').removeClass('active');
+        $(`li[data-itemid=${currActiveId}]`).addClass('active');
+    }
+
 });
--- src/Screen.js
+++ src/Screen.js
@@ -37,7 +37,13 @@ const Screen = () => {
     return (
         <div id="screen">
             <div id="menu">
-                iPod.js
+                <span style={{padding: 5, fontSize: '1.3em'}}>iPod.js</span>
+                <ul className="menu-items-list">
+                    <li className="menu-item" data-itemid="0">Cover Flow</li>
+                    <li className="menu-item active" data-itemid="1">Music</li>
+                    <li className="menu-item" data-itemid="2">Games</li>
+                    <li className="menu-item" data-itemid="3">Settings</li>
+                </ul>
             </div>
         </div>
     );
